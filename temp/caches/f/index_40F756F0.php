290b80c311cd31d7b9610936af50344a8870a6d2
allenlu1991 <903949263@qq.com>
2015-04-03 16:48:58 +0800

    v1.0

--- temp/caches/f/index_40F756F0.php
+++ temp/caches/f/index_40F756F0.php
@@ -1,4 +1,4 @@
-<?php exit;?>a:3:{s:8:"template";a:19:{i:0;s:46:"F:/wamp/www/circlemei/themes/miqinew/index.dwt";i:1;s:66:"F:/wamp/www/circlemei/themes/miqinew/library/page_header_index.lbi";i:2;s:68:"F:/wamp/www/circlemei/themes/miqinew/library/category_tree_index.lbi";i:3;s:57:"F:/wamp/www/circlemei/themes/miqinew/library/index_ad.lbi";i:4;s:59:"F:/wamp/www/circlemei/themes/miqinew/library/index_ad_r.lbi";i:5;s:59:"F:/wamp/www/circlemei/themes/miqinew/library/index_ad_b.lbi";i:6;s:62:"F:/wamp/www/circlemei/themes/miqinew/library/pinpaiqiehuan.lbi";i:7;s:58:"F:/wamp/www/circlemei/themes/miqinew/library/group_buy.lbi";i:8;s:62:"F:/wamp/www/circlemei/themes/miqinew/library/recommend_hot.lbi";i:9;s:68:"F:/wamp/www/circlemei/themes/miqinew/library/recommend_promotion.lbi";i:10;s:62:"F:/wamp/www/circlemei/themes/miqinew/library/recommend_new.lbi";i:11;s:63:"F:/wamp/www/circlemei/themes/miqinew/library/recommend_best.lbi";i:12;s:58:"F:/wamp/www/circlemei/themes/miqinew/library/cat_goods.lbi";i:13;s:54:"F:/wamp/www/circlemei/themes/miqinew/library/top10.lbi";i:14;s:59:"F:/wamp/www/circlemei/themes/miqinew/library/email_list.lbi";i:15;s:60:"F:/wamp/www/circlemei/themes/miqinew/library/order_query.lbi";i:16;s:64:"F:/wamp/www/circlemei/themes/miqinew/library/huiyuanchongzhi.lbi";i:17;s:62:"F:/wamp/www/circlemei/themes/miqinew/library/index_history.lbi";i:18;s:60:"F:/wamp/www/circlemei/themes/miqinew/library/page_footer.lbi";}s:7:"expires";i:1427645797;s:8:"maketime";i:1427642197;}<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<?php exit;?>a:3:{s:8:"template";a:19:{i:0;s:57:"/a/domains/wx.wawow.me/circlemei/themes/miqinew/index.dwt";i:1;s:77:"/a/domains/wx.wawow.me/circlemei/themes/miqinew/library/page_header_index.lbi";i:2;s:79:"/a/domains/wx.wawow.me/circlemei/themes/miqinew/library/category_tree_index.lbi";i:3;s:68:"/a/domains/wx.wawow.me/circlemei/themes/miqinew/library/index_ad.lbi";i:4;s:70:"/a/domains/wx.wawow.me/circlemei/themes/miqinew/library/index_ad_r.lbi";i:5;s:70:"/a/domains/wx.wawow.me/circlemei/themes/miqinew/library/index_ad_b.lbi";i:6;s:73:"/a/domains/wx.wawow.me/circlemei/themes/miqinew/library/pinpaiqiehuan.lbi";i:7;s:69:"/a/domains/wx.wawow.me/circlemei/themes/miqinew/library/group_buy.lbi";i:8;s:73:"/a/domains/wx.wawow.me/circlemei/themes/miqinew/library/recommend_hot.lbi";i:9;s:79:"/a/domains/wx.wawow.me/circlemei/themes/miqinew/library/recommend_promotion.lbi";i:10;s:73:"/a/domains/wx.wawow.me/circlemei/themes/miqinew/library/recommend_new.lbi";i:11;s:74:"/a/domains/wx.wawow.me/circlemei/themes/miqinew/library/recommend_best.lbi";i:12;s:69:"/a/domains/wx.wawow.me/circlemei/themes/miqinew/library/cat_goods.lbi";i:13;s:65:"/a/domains/wx.wawow.me/circlemei/themes/miqinew/library/top10.lbi";i:14;s:70:"/a/domains/wx.wawow.me/circlemei/themes/miqinew/library/email_list.lbi";i:15;s:71:"/a/domains/wx.wawow.me/circlemei/themes/miqinew/library/order_query.lbi";i:16;s:75:"/a/domains/wx.wawow.me/circlemei/themes/miqinew/library/huiyuanchongzhi.lbi";i:17;s:73:"/a/domains/wx.wawow.me/circlemei/themes/miqinew/library/index_history.lbi";i:18;s:71:"/a/domains/wx.wawow.me/circlemei/themes/miqinew/library/page_footer.lbi";}s:7:"expires";i:1428044383;s:8:"maketime";i:1428040783;}<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta name="Generator" content="ECSHOP v2.7.3" />
@@ -156,7 +156,8 @@ catch (e) {
       </div>
     </div>
   </div>
-</div><div class="block">
+</div>
+<div class="block">
   <div class="content">
     <div class="contentBody">
       <div class="mainBody"> <div class="left_nav">
@@ -370,7 +371,8 @@ catch (e) {
         </div>
         
         
-      </div>        <div class="right_banner"> <div id="slideBox" class="slideBox">
+      </div>        <div class="right_banner">  
+<div id="slideBox" class="slideBox">
   <div class="hd">
     <ul>
       <li class="first"> </li>
@@ -393,7 +395,8 @@ catch (e) {
 </div>
 <script type="text/javascript">
 		jQuery(".slideBox").slide({mainCell:".bd ul",effect:"left",trigger:"click"});
-		</script>         <div class="banner_ico">
+		</script> 
+          <div class="banner_ico">
                 	<a href="#" target="_blank"></a>
                     <a href="#" target="_blank"></a>
                     <a href="#" target="_blank"></a>
@@ -418,9 +421,11 @@ catch (e) {
   <div class="big_logo" id="banner_04"> <a href="#" target="_blank"><img src="themes/miqinew/images/b4.jpg" border="0"></a>
     <div class="big_txt" > <a href="#" target="_blank"><img src="themes/miqinew/images/b4_1.png" border="0"></a> </div>
   </div>
-</div> </div>
+</div>
+ </div>
       </div>
-      <div class="mainBody"> <div class="slideTxtBox">
+      <div class="mainBody"> 
+<div class="slideTxtBox">
   <div class="hd">
     <ul>
       <li> <b>欧美品牌</b>
@@ -493,7 +498,8 @@ catch (e) {
 </div>
 <script type="text/javascript">
 		jQuery(".slideTxtBox").slide();
-		</script> </div>
+		</script> 
+ </div>
       <div class="mainBody"> <div class="picScroll-left">
   <div class="hd">
     <div class="title_txt"> <img src="themes/miqinew/images/title_txt01.jpg" border="0"> </div>
@@ -860,6 +866,7 @@ catch (e) {
       <div class="sucess_joinCart" id="addtocartdialog_retui_122_new">
 </div>
 </dd>
+ 
  
               
    <dd class="hot_dd02" >
@@ -1983,7 +1990,8 @@ function check_email()
     return false;
   }
 }
-</script><script>var invalid_order_sn = "无效订单号"</script>
+</script>
+<script>var invalid_order_sn = "无效订单号"</script>
 <div class="f_l email">
  
   <div class="batch_title"><p>订单查询</p></div>
@@ -2007,7 +2015,8 @@ function check_email()
        
        
       
-      <div class="vip_right">
+      
+<div class="vip_right">
   <div class="vip_title">
     <p>会员充值<span>（享受VIP 专属7大特权）</span></p>
   </div>
@@ -2058,7 +2067,8 @@ function check_email()
 		jQuery(".slideTxtBox2").slide();
 		</script>
   <div class="vip_btn"> <a href="/user.php?act=register"></a> </div>
-</div> 
+</div>
+ 
       
     </div>
   </div>
@@ -2228,7 +2238,7 @@ function clear_history_Response(res)
     <br />
      
     554fcae493e564ee0dc75bdf2ebf94caquery_info|a:1:{s:4:"name";s:10:"query_info";}554fcae493e564ee0dc75bdf2ebf94ca<br />
-    <a href="http://www.ecshop.com" target="_blank" style=" font-family:Verdana; font-size:11px;">Powered&nbsp;by&nbsp;<strong><span style="color: #3366FF">ECShop</span>&nbsp;<span style="color: #FF9966">v2.7.3</span></strong></a>&nbsp;<a href="http://www.ecshop.com/license.php?product=ecshop_b2c&url=http%3A%2F%2Flocalhost%2Fcirclemei%2F" target="_blank"
+    <a href="http://www.ecshop.com" target="_blank" style=" font-family:Verdana; font-size:11px;">Powered&nbsp;by&nbsp;<strong><span style="color: #3366FF">ECShop</span>&nbsp;<span style="color: #FF9966">v2.7.3</span></strong></a>&nbsp;<a href="http://www.ecshop.com/license.php?product=ecshop_b2c&url=http%3A%2F%2Fcirclemei.wawow.me%2F" target="_blank"
 >&nbsp;&nbsp;Licensed</a><br />
         <div align="left"  id="rss"><a href="feed.php"><img src="themes/miqinew/images/xml_rss2.gif" alt="rss" /></a></div>
   </div>
