20b6cb48a1dcc329c51220aab9ecf0041e8e28ad
allenlu1991 <903949263@qq.com>
2015-04-03 15:57:23 +0800

    v1.0

--- temp/compiled/position_get_adv_small.lbi.php
+++ temp/compiled/position_get_adv_small.lbi.php
@@ -1,7 +1,12 @@
 <div class="hd">
       <ul>
+<<<<<<< HEAD
       <?php $_from = $this->_var['ad_child']; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array'); }; $this->push_vars('', 'child_0_47449600_1428042091');if (count($_from)):
     foreach ($_from AS $this->_var['child_0_47449600_1428042091']):
+=======
+      <?php $_from = $this->_var['ad_child']; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array'); }; $this->push_vars('', 'child_0_26908400_1428045705');if (count($_from)):
+    foreach ($_from AS $this->_var['child_0_26908400_1428045705']):
+>>>>>>> develop
 ?>
         <li></li>
         <?php endforeach; endif; unset($_from); ?><?php $this->pop_vars();; ?>  
@@ -9,10 +14,17 @@
     </div>
     <div class="bd">
      <ul>
+<<<<<<< HEAD
 		<?php $_from = $this->_var['ad_child']; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array'); }; $this->push_vars('', 'child_0_47504100_1428042091');if (count($_from)):
     foreach ($_from AS $this->_var['child_0_47504100_1428042091']):
 ?>
             <li><a href="<?php echo $this->_var['child_0_47504100_1428042091']['ad_link']; ?>" target="_blank"><img src="data/afficheimg/<?php echo $this->_var['child_0_47504100_1428042091']['ad_code']; ?>" alt="" class="goodsimg" /></a></li>
+=======
+		<?php $_from = $this->_var['ad_child']; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array'); }; $this->push_vars('', 'child_0_26941500_1428045705');if (count($_from)):
+    foreach ($_from AS $this->_var['child_0_26941500_1428045705']):
+?>
+            <li><a href="<?php echo $this->_var['child_0_26941500_1428045705']['ad_link']; ?>" target="_blank"><img src="data/afficheimg/<?php echo $this->_var['child_0_26941500_1428045705']['ad_code']; ?>" alt="" class="goodsimg" /></a></li>
+>>>>>>> develop
         <?php endforeach; endif; unset($_from); ?><?php $this->pop_vars();; ?>                             	
     </ul>
     </div>
--- temp/query_caches/sqlcache_config_file_6f5ee472e2d8fc4c32f8c2156b6b3e26.php
+++ temp/query_caches/sqlcache_config_file_6f5ee472e2d8fc4c32f8c2156b6b3e26.php
@@ -1,5 +1,9 @@
 <?php
+<<<<<<< HEAD
 $this->mysql_config_cache_file_time = 1428042088;
+=======
+$this->mysql_config_cache_file_time = 1428045704;
+>>>>>>> develop
 $this->timeline = 0;
 $this->timezone = 0;
 $this->platform = 'OTHER';
